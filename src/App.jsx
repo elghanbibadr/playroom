e1ea6d94c9485d7224cb211ea3361969f15c2774
elghanbibadr <bghanbi50@gmail.com>
2024-01-29 12:17:35 +0100

    created the first version of the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
-
+import Home from './pages/Home'
+// import Navbar from './componenets/Navbar'
+import Navbar from './componenets/Navbar'
 const App = () => {
   return (
     <div className='text-white'>
-      <h1 className='text-[5rem] leading-[50px] tracking-[-1%] font-semibold lg:text-[6.8rem] lg:leading-[68px]'>Build multiplayer games in minutes</h1>
-      <h2 className='text-[3rem] leading-[30px] font-medium lg:text-[4.8rem] lg:leading-[48px]'>Built for the next generation of multiplayer </h2>
-      <p className='text-[1.4rem] leading-[20px] font-light text-textGrey md:text-[1.8rem] md:leading-[26px]'>
-      Multiplayer configurations and tools to create games previously only feasible by large-scale teams now possible by EVERYONE.
-
-      </p>
+     <Home />
     </div>
   )
 }
