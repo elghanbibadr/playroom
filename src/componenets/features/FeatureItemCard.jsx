9136ec6a594735792900452c2d3849d5264725d4
elghanbibadr <bghanbi50@gmail.com>
2024-01-31 20:37:01 +0100

    refining the features section

--- src/componenets/features/FeatureItemCard.jsx
+++ src/componenets/features/FeatureItemCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Card from '../ui/Card'
 import moderationIcon from "../../assets/moderationIcon.svg"
-const FeatureItemCard = () => {
+const FeatureItemCard = ({imgSrc,title,subheading}) => {
   return (
-    <Card className="pt-10 px-12 md:pb-10 h-[150px] md:h-[180px] flex flex-col items-start justify-end md:w-full">
-        <img className='h-10 mb-2' src={moderationIcon} alt="" />
-        <h6 className='my-2 text-[1.6rem] font-bold'>Moderation</h6>
+    <Card className="pt-10 carousel-item w-[170px]  md:m-6 px-12 md:pb-10 h-[150px] md:h-[180px] flex flex-col items-start justify-end md:w-full">
+        <img className='h-12 mb-2' src={imgSrc} alt="feature icon" />
+        <h6 className='my-2 text-[1.6rem] font-bold'>{title}</h6>
         <span className='hidden md:block text-[1.4rem] text-smallTextColor'>
-        Subheading about the feature here
+        {subheading}
         </span>
     </Card>
   )
--- src/componenets/features/Features.jsx
+++ src/componenets/features/Features.jsx
@@ -1,4 +1,49 @@
 import FeatureItemCard from "./FeatureItemCard"
+import joystickIcon from "../../assets/joystickIcon.svg"
+import lobyIcon from "../../assets/lobyIcon.svg"
+import moderationIcon from "../../assets/moderationIcon.svg"
+import gamepadIcon from "../../assets/gamepad.svg"
+import playtestingIcon from "../../assets/playtestting.svg"
+import chatIcon from "../../assets/chat.svg"
+
+const featureCardsData=[
+    {
+        id:1,
+        title:"Joysticks",
+        subheading:"Subheading about the feature here",
+        imgSrc:joystickIcon
+    },
+    {
+        id:2,
+        title:"Lobby",
+        subheading:"Subheading about the feature here",
+        imgSrc:lobyIcon
+    },
+    {
+        id:3,
+        title:"Moderation",
+        subheading:"Subheading about the feature here",
+        imgSrc:moderationIcon
+    },
+    {
+        id:4,
+        title:"Gamepads",
+        subheading:"Subheading about the feature here",
+        imgSrc:gamepadIcon
+    },
+    {
+        id:5,
+        title:"Playtesting",
+        subheading:"Subheading about the feature here",
+        imgSrc:playtestingIcon
+    },
+    {
+        id:6,
+        title:"Chat",
+        subheading:"Subheading about the feature here",
+        imgSrc:chatIcon
+    },
+]
 const Features = () => {
   return (
     <div className='p-4'>
@@ -9,13 +54,11 @@ const Features = () => {
         </p>
         <button className='text-black mt-6 bg-white  rounded-full p-6 px-10 text-[1.1rem] md:text-[1.4rem] font-medium'>Read Documentation</button>
       </div>
-      <div className="flex gap-4 mt-10 md:mt-20 md:grid md:grid-cols-3 md:gap-20 ">
-          <FeatureItemCard/>
-          <FeatureItemCard/>
-          <FeatureItemCard/>
-          <FeatureItemCard/>
-          <FeatureItemCard/>
-          <FeatureItemCard/>
+      <div className="flex carousel rounded-box md:p-20 gap-4 mt-10 md:mt-20 md:grid md:grid-cols-3 md:gap-x-28 ">
+          {featureCardsData.map(({id,imgSrc,title,subheading}) =>{
+          return <FeatureItemCard key={id} imgSrc={imgSrc} title={title} subheading={subheading} />
+          })}
+         
   
       </div>
     </div>
