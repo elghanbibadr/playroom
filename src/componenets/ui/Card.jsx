341de159fcfc63047e53b6096c88089a3b8eb83d
elghanbibadr <bghanbi50@gmail.com>
2024-02-01 21:41:12 +0100

    fix card width issue

--- src/componenets/ui/Card.jsx
+++ src/componenets/ui/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({children,className}) => {
   return (
-    <div className={`bg-lightBlack   w-fit p-4 rounded-[18px] border-[0.67px] border-primaryBorderColor ${className}`}>{children}</div>
+    <div className={`bg-lightBlack   p-4 rounded-[18px] border-[0.67px] border-primaryBorderColor ${className}`}>{children}</div>
   )
 }
 
