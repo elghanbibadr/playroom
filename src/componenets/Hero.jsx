e37aaeff1d449a897953f6f9db853008eff748d0
elghanbibadr <bghanbi50@gmail.com>
2024-02-03 12:13:15 +0100

    more fixes

--- src/componenets/Hero.jsx
+++ src/componenets/Hero.jsx
@@ -3,7 +3,7 @@ import Button from "./ui/Button"
 const Hero = () => {
   return (
     // md:mt-40  pb-20 mb-[160px]
-    <div className='md:text-center  mb-20 md:mt-[20vh]  '>
+    <div className='md:text-center  mb-20 md:mt-[17vh]  '>
     <div>
         <div className='flex text-nowrap items-center border-[1px] p-2 md:py-1 md:px-12 lg:px-16 rounded-full w-full mt-6 md:w-fit md:mx-auto justify-between  px-8 border-[#2D2D2D]'>
         <p className='text-[1rem] font-normal md:mr-4 md:text-[1.2rem]'>Unity release with v <span className="hidden md:inline-block">reset states</span> 0.1.0</p>
@@ -13,7 +13,7 @@ const Hero = () => {
     {/* mt-40 */}
     <div className='mt-20  md:mt-4 p-5'>
       <h1>Build multiplayer <br className='hidden md:block' /> games in minutes</h1>
-      <p className='mt-4 md:my-[1vh]'>
+      <p className='mt-4 md:my-[3vh]'>
       Playroom provides scalable Multiplayer Backend & Cross-platform <br className='hidden md:block' /> integrations for web-based games with Zero Server code. 
       </p>
     </div>
--- src/componenets/ui/Button.jsx
+++ src/componenets/ui/Button.jsx
@@ -1,11 +1,8 @@
-import { motion } from "framer-motion"
 
 const Button = ({children,className}) => {
   return (
-    <motion.button  
-    whileHover={{ scale: 1.1 }}
-    whileTap={{ scale: 0.9 }}
-    transition={{ type: "spring", stiffness: 400, damping: 17 }}className={`box text-black mt-6 gradientBtn  rounded-full p-6 px-16  text-[1.2rem] md:text-[1.3rem] font-[500] ${className}`}>{children}</motion.button>
+    <button  
+   className={`box text-black mt-6 gradientBtn  rounded-full p-6 px-16  text-[1.2rem] md:text-[1.3rem] font-[500] ${className}`}>{children}</button>
   )
 }
 
