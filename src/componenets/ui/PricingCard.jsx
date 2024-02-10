7a02d88e69fb1022c4654369e52215799354c88d
elghanbibadr <bghanbi50@gmail.com>
2024-02-10 18:08:29 +0100

    created the v1 of the prricing page table

--- src/componenets/ui/PricingCard.jsx
+++ src/componenets/ui/PricingCard.jsx
@@ -2,18 +2,23 @@ import Card from "./Card"
 import Button from "./Button"
 import checkIcon from "../../assets/checkIcon.svg"
 
-const PricingCard = () => {
+const PricingCard = ({title,price,buttonName,planFeatures}) => {
   return (
-    <Card className="rounded-[17px] py-10">
-        <span className="text-white font-bold">Hobby</span>
-        <h3 className="text-[27px] font-semibold">Custom</h3>
-        <button className="rounded-[6px] bg-transparent w-full my-10 p-4 text-white border-[1px] border-[#292A2D]">Get in Touch</button>
+    <Card className={`rounded-[17px] py-14  ${price === "Custom" ? "bg-[#1B1125]":""}  px-8 my-4`}>
+        <div className="md:text-left">
+            <span className="text-white text-[14px] font-semibold">{title}</span>
+            <h3 className="text-[27px] font-semibold capitalize">{price}</h3>
+        </div>
+        <button className={`rounded-[6px]  text-[14px] bg-transparent w-full my-8 p-4  border-[1px] border-[#292A2D] ${price.toLowerCase() === "free" ? "gradientBtn text-black" : "text-white"} `}>{buttonName}</button>
         <ul>
-            <li className="flex ">
+            {planFeatures.map((feature,index) =>{
+                return  <li key={index} className="flex ">
                 <img className="mr-3" src={checkIcon} alt="check icon" />
-                <span>Non Commercial</span>
+                <span className="my-2 text-[14px]">{feature}</span>
 
-            </li>
+            </li> 
+            })}
+           
         </ul>
     </Card>
   )
--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -2,10 +2,175 @@
 import Navbar from "../../componenets/Navbar"
 import Button from "../../componenets/ui/Button"
 import PricingCard from "../../componenets/ui/PricingCard"
+import { Container } from "../../componenets/ui/Container"  
+import PackagesList from "../../componenets/multiplayer/MultiPlayerList"
+import checkIcon from "../../assets/checkIcon.svg"
+import checkIconWhite from "../../assets/checkIconWhite.png"
+import checkIconGrey from "../../assets/checkIcongrey.svg"
+import xmark from "../../assets/xmark.svg"
+
+
+const plansData=[
+  {
+    id:1,
+    title:"Hobby",
+    price:"free",
+    buttonName:"Get started",
+    planFeatures:["Non Commercial","Unlimited CCU"]
+  },
+  {
+    id:2,
+    title:"Starter",
+    price:"$10",
+    buttonName:"Get in Touch",
+    planFeatures:["10k MAU (Monthly Active Users)","Unlimited CCU","Auto Scaling","$0.0075 additional users"]
+  },
+  {
+    id:3,
+    title:"Studio",
+    price:"$150",
+    buttonName:"Get in Touch",
+    planFeatures:["200k MAU (Monthly Active Users)","Unlimited CCU","Auto Scaling","$0.0050 additional users","Priority Support"]
+  },
+  {
+    id:4,
+    title:"Enterprise",
+    price:"Custom",
+    buttonName:"Contact Us",
+    planFeatures:["Multiple Games","1M+ MAU","Custom Pricing","Unlimited CCU","Team Dashboard","Dedicated support"]
+  },
+]
+
+
+const packageOffresData1 = [
+  { text: "CCU Limit", hasCheck: false, iconSource: "path/to/ccu_limit_icon.png" },
+  { text: "Platforms", hasCheck: false, iconSource: "path/to/platforms_icon.png" },
+  { text: "Managed SSL Certificates", hasCheck: false, iconSource: "path/to/ssl_certificates_icon.png" },
+  { text: "Bandwidth", hasCheck: false, iconSource: "path/to/bandwidth_icon.png" },
+  { text: "Support", hasCheck: false, iconSource: "path/to/support_icon.png" },
+  { text: "Scaling", hasCheck: false, iconSource: "path/to/scaling_icon.png" },
+  { text: "Region", hasCheck: false, iconSource: "path/to/region_icon.png" },
+  { text: "Updates", hasCheck: false, iconSource: "path/to/updates_icon.png" },
+  { text: "Bug Handling", hasCheck: false, iconSource: "path/to/bug_handling_icon.png" },
+  { text: "Documentation", hasCheck: false, iconSource: "path/to/documentation_icon.png" }
+];
+const packageOffresData2 = [
+  { text: "Open Source Templates", hasCheck: false, iconSource: null },
+  { text: "UI Lobby", hasCheck: false, iconSource: null },
+  { text: "UI Matchmaking", hasCheck: false, iconSource:null },
+  { text: "Sync’d Game Objects", hasCheck: false, iconSource:null },
+  { text: "Gamepads Integrations", hasCheck: false, iconSource:null},
+  { text: "UI Avatar pickers", hasCheck: false, iconSource: null },
+ 
+  
+];
+const packageOffresData3 = [
+  { text: "Sync’d Chat Kit", hasCheck: false, iconSource: null },
+  { text: "Sync’d Voice Kit", hasCheck: false, iconSource: null },
+  { text: "Stream mode UI", hasCheck: false, iconSource:null },
+  { text: "Tv mode UI", hasCheck: false, iconSource:null },
+  { text: "Twitch Moderation Kit", hasCheck: false, iconSource:null},
+  { text: "AI Assets Generator API", hasCheck: false, iconSource: null },
+  { text: "AI Text/Voice generatior with OpenAI", hasCheck: false, iconSource: null },
+  { text: "25+ Customizable Multiplayer Objects", hasCheck: false, iconSource: null },
+ 
+  
+];
+const packageOffresData4 = [
+  { text: "Ready Player Me Integration", hasCheck: false, iconSource: null },
+  { text: "Discord Integration", hasCheck: false, iconSource: null },
+  { text: "Bunch Integration", hasCheck: false, iconSource:null },
+  { text: "Twitch Integration", hasCheck: false, iconSource:null },
+  { text: "TikTok Integration", hasCheck: false, iconSource:null},
+  { text: "Crazygames Integration", hasCheck: false, iconSource: null },
+  { text: "Payment partners", hasCheck: false, iconSource: null },
+  { text: "Auth partners", hasCheck: false, iconSource: null },
+  { text: "Analytics partners", hasCheck: false, iconSource: null },
+ 
+  
+];
+const packageOffresData5 = [
+  { text: "Unlimited", hasCheck: false, iconSource: null },
+  { text: "Ship Everywhere", hasCheck: false, iconSource: null },
+  { text: null, hasCheck: true, iconSource:checkIcon },
+  { text: "Auto Scale", hasCheck: false, iconSource:null },
+  { text: null, hasCheck: true, iconSource:checkIcon },
+  { text: null, hasCheck: true, iconSource:checkIcon },
+  { text: "Global", hasCheck: false, iconSource: null },
+  { text: null, hasCheck: true, iconSource:checkIcon },
+  { text: null, hasCheck: true, iconSource:checkIcon },
+  { text: "The Best!", hasCheck: false, iconSource: null },
+ 
+  
+];
+const packageOffresData6 = [
+  { text: null, hasCheck: true, iconSource:checkIcon },
+  { text: null, hasCheck: true, iconSource:checkIcon },
+  { text: null, hasCheck: true, iconSource:checkIcon },
+  { text: null, hasCheck: true, iconSource:checkIcon },
+  { text: null, hasCheck: true, iconSource:checkIcon },
+  { text: null, hasCheck: true, iconSource:checkIcon },
+ 
+  
+];
+const packageOffresData7 = [
+  { text: "Alpha", hasCheck: false, iconSource: null },
+  { text: "Alpha", hasCheck: false, iconSource: null },
+  { text: "Alpha", hasCheck: false, iconSource: null },
+  { text: "Alpha", hasCheck: false, iconSource: null },
+  { text: "Alpha", hasCheck: false, iconSource: null },
+  { text: "Alpha", hasCheck: false, iconSource: null },
+  { text: "Alpha", hasCheck: false, iconSource: null },
+  { text: "More coming soon", hasCheck: false, iconSource: null },
+];
+const packageOffresData8= [
+  { text: "Alpha", hasCheck: false, iconSource: null },
+  { text: "Alpha Live", hasCheck: false, iconSource: null },
+  { text: "In Development", hasCheck: false, iconSource: null },
+  { text: "Alpha Live", hasCheck: false, iconSource: null },
+  { text: "TBD", hasCheck: false, iconSource: null },
+  { text: "TBD", hasCheck: false, iconSource: null },
+  { text: "TBD", hasCheck: false, iconSource: null },
+  { text: "TBD", hasCheck: false, iconSource: null },
+
+];
+
+const packageOffresData9= [
+  { text: "CCU Limit pricing", hasCheck: false, iconSource: null },
+  { text: "per platfom pricing", hasCheck: false, iconSource: null },
+  { text: null, hasCheck: true, iconSource:checkIconGrey },
+  { text: "CPU and Bandwidth charges", hasCheck: false, iconSource: null },
+  { text: "Only if you are enterprise", hasCheck: false, iconSource: null },
+  { text: "vertical and horizontal pricing", hasCheck: false, iconSource: null },
+  { text: "Per Region server pricing", hasCheck: false, iconSource: null },
+  { text: null, hasCheck: true, iconSource:checkIconGrey },
+  { text: null, hasCheck: true, iconSource:checkIconGrey },
+  { text: "Good luck!", hasCheck: false, iconSource: null },
+
+];
+const packageOffresData10= [
+  { text: "Requires setup", hasCheck: false, iconSource: null },
+  { text: null, hasCheck: true, iconSource:checkIconGrey },
+  { text: null, hasCheck: true, iconSource:checkIconGrey },
+  { text: null, hasCheck: true, iconSource:checkIconGrey },
+  { text: null, hasCheck: true, iconSource:checkIconGrey },
+  { text: null, hasCheck: true, iconSource:checkIconGrey },
+];
+
+const packageOffresData11= [
+  { text: null, hasCheck: true, iconSource:xmark },
+  { text: null, hasCheck: true, iconSource:xmark },
+  { text: null, hasCheck: true, iconSource:xmark },
+  { text: null, hasCheck: true, iconSource:xmark },
+  { text: null, hasCheck: true, iconSource:xmark },
+  { text: null, hasCheck: true, iconSource:xmark },
+];
+
+
 
 const Pricing = () => {
   return (
-    <div className="bg-black p-4">
+    <Container className="bg-black p-4">
         <Navbar/>
         <div className='mt-32 text-left md:text-center  p-5'>
       <h2>Simple Pricing </h2>
@@ -13,9 +178,48 @@ const Pricing = () => {
       Transparent pricing based on MAU (Monthly active users). Pay Zero for non-commercial and low-usage games.
       </p>   
        <Button>Price Comparison</Button>
-       <PricingCard/>
+       {/* custom-grid */}
+       <div className="mt-20 gap-6 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
+         {plansData.map((plan,index) =>{
+         return <PricingCard key={index} {...plan}/>
+         })}
        </div>
-    </div>
+       </div>
+
+       {/* MULTIPLE PLAYER */}
+       <div className="text-center mt-40">
+         <h2>Multiplayer & More </h2>
+               <p className='mt-4 md:my-[3vh]'>
+               Building multiplayer is hard and its more than just networking. Leverage Playroom to you ship your games faster and across platforms.
+               </p>
+
+
+              <div className="md:grid mt-20 md:mt-40  md:grid-cols-3 ">
+                <div>
+                  <PackagesList packageOffres={packageOffresData1} packageName="Multiplayer Services" />
+                  <PackagesList packageOffres={packageOffresData2} packageName="Basic Modules & UI Kits" />
+                  <PackagesList packageOffres={packageOffresData3} packageName="Pro Modules & UI Kits (Usage based pricing)" />
+                  <PackagesList packageOffres={packageOffresData4} packageName="Cross-platform Integrations (Custom pricing)" />
+                </div>
+                <div className="bg-[#1B1125]  rounded-3xl h-full">
+                  {/* <h1>hello world</h1> */}
+                  <PackagesList className=" text-white" packageOffres={packageOffresData5} packageName="Playroom" hasBorder={false} />
+                  <PackagesList className=" text-white py-20" packageOffres={packageOffresData6} packageName=" " hasBorder={false}  />
+                  <PackagesList className=" text-white py-20" packageOffres={packageOffresData7} packageName=" " hasBorder={false}  />
+                  <PackagesList className=" text-white py-20" packageOffres={packageOffresData8} packageName=" " hasBorder={false}  />
+
+                </div>
+                <div>
+                  <PackagesList packageOffres={packageOffresData9} packageName="Others" />
+                  <PackagesList packageOffres={packageOffresData10} packageName="" />
+                  <PackagesList packageOffres={packageOffresData11} packageName="" />
+                  <PackagesList packageOffres={packageOffresData11} packageName="" />
+                </div>
+              </div>
+       </div>
+
+       {/* MULTIPLE PLAYER */}
+    </Container>
   )
 }
 
