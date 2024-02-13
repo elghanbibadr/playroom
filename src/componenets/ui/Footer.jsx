ab721f81d013f6c2b53804fb756f9be2522e30c1
elghanbibadr <bghanbi50@gmail.com>
2024-02-13 16:30:45 +0100

    more fix

--- src/componenets/ui/Footer.jsx
+++ src/componenets/ui/Footer.jsx
@@ -3,8 +3,8 @@ import footerShadowLightImg from "../../assets/footerLightShadow.svg"
 
 export const Footer = () => {
   return (
-    <footer>
-  <img className='mt-40 hidden md:block relative top-10' src={footerShadowLightImg} alt="footer shadow light image" />
+    <footer >
+  <img className='mt-40 hidden md:block relative ' src={footerShadowLightImg} alt="footer shadow light image" />
 <svg className='md:hidden' viewBox="0 0 393 169" fill="none" xmlns="http://www.w3.org/2000/svg">
 <rect x="-868.789" y="0.711914" width="1366" height="175.965" fill="url(#paint0_linear_1_1202)"/>
 <defs>
