12cfd4075b65c22d9a129d0c9260939dd5a51e3a
elghanbibadr <bghanbi50@gmail.com>
2024-02-14 15:26:19 +0100

    change border to 20px

--- src/componenets/ui/NavLink.jsx
+++ src/componenets/ui/NavLink.jsx
@@ -2,7 +2,7 @@
 
 const NavItem = ({handleItemClick,clickedItem,children,itemIndex}) => {
   return (
-    <li  className={`rounded-full my-3 md:my-0 md:p-2  hover:text-[#efefefc8] border-lightBlack md:border-[1px] transition-colors duration-150 md:py-4 md:px-6 lg:py-5 lg:px-11  ${clickedItem === itemIndex ? 'md:border-[#8C72F4]  ' : ''}`} onMouseEnter={() => handleItemClick(itemIndex)}  onClick={() => handleItemClick(itemIndex)}>{children}</li>
+    <li  className={`rounded-[20px] my-3  md:my-0 md:p-2  hover:text-[#efefefc8] border-lightBlack md:border-[1px] transition-colors duration-150 md:py-4 md:px-6 lg:py-5 lg:px-11  ${clickedItem === itemIndex ? 'md:border-[#8C72F4]  ' : ''}`} onMouseEnter={() => handleItemClick(itemIndex)}  onClick={() => handleItemClick(itemIndex)}>{children}</li>
 
   )
 }
