3232b4249b6a8452f99974a897ebc01de5ea24d4
elghanbibadr <bghanbi50@gmail.com>
2024-02-20 17:50:29 +0100

    fix alignemnt issue and make the multiplayer section title and desc also sticky

--- src/componenets/PricingPageComponenets/PackageList.jsx
+++ src/componenets/PricingPageComponenets/PackageList.jsx
@@ -1,7 +1,7 @@
 
 const PackageList = ({index,packageOffres,packageName,className,hasBorder=true}) => {
   return (
-    <div className={`md:my-20   my-20 text-[#D2D2D2] ${className}  ${(packageName === 'Multiplayer Services'  || packageName ===  'Others') ? "h-[360px]":(packageOffres[0].text ==="Requires setup"  || packageName ===  'Basic Modules & UI Kits') ? "h-[270px]":"h-[350px]"} `}>
+    <div className={`md:my-20   my-20 text-[#D2D2D2] ${className}  ${(packageName === 'Multiplayer Services'  || packageName ===  'Others') ? "h-[360px]":(packageOffres[0].text ==="Requires setup"  || packageName ===  'Basic Modules & UI Kits') ? "h-[270px]":""} h-[400px] `}>
     <h6 className={` h-14  text-[13px] font-semibold ${hasBorder ? "border-b-[1.5px] pb-6 border-[#8f8d8d73] text-[#D2D2D2]" : "text-white text-[2.3rem]"} ${(packageName === 'Multiplayer Services' || packageName === 'Playroom' || packageName === 'Others') ? 'md:hidden':''} `}>{packageName}</h6>
     {/* <h6 className={` h-14  text-[13px] font-semibold ${hasBorder ? "border-b-[1.5px] pb-6 border-[#8f8d8d73] text-[#D2D2D2]" : "text-white text-[2.3rem]"} ${index === 0 ? 'sticky top-0' :''} `}>{packageName}</h6> */}
 
--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -12,27 +12,29 @@ const Pricing = () => {
           return <PricingCard key={index} {...plan} />
         })}
       </div>
-      <div className="text-center mt-40">
-        <h2>Multiplayer & More </h2>
-        <p className='mt-4 md:my-[3vh]'>
-          Building multiplayer is hard and its more than just networking. Leverage <br className='hidden md:block' />  Playroom to you ship your games faster and across platforms.
-        </p>
+      <div className="text-center mt-40 ">
+        <div className="md:sticky  md:bg-black  md:top-0 md:bottom-6 z-10 ">
+          <h2>Multiplayer & More </h2>
+          <p className='mt-4 md:my-[3vh] '>
+            Building multiplayer is hard and its more than just networking. Leverage <br className='hidden md:block' />  Playroom to you ship your games faster and across platforms.
+          </p>
+        </div>
        
         {/* mt-20 md:mt-40 */}
-        <div className="md:grid sm:w-[80%] sm:mx-auto mt-20 md:my-40 md:w-full lg:w-[70%]    md:grid-cols-3   ">
+        <div className="md:grid   sm:w-[80%] sm:mx-auto  md:my-40 md:w-full lg:w-[70%]    md:grid-cols-3   ">
      
           <div>
             {packageOffersData1.map(({ packageName, packageFeatures }, index) => {
               return <> 
-                {index === 0 && <div className="hidden md:sticky md:block bg-black md:top-0 md:pt-8 border-b-[1.5px] pb-6 border-[#8f8d8d73]">
+                {index === 0 && <div className="hidden md:sticky md:block  bg-black md:top-[10.3rem] md:pt-8 border-b-[1.5px] pb-6 border-[#8f8d8d73]">
                 <h6 className="text-left  text-[13px] text-[#D2D2D2] ">Multiplayer Services</h6>
                 </div> }<PackageList key={index} className='text-left' packageOffres={packageFeatures} packageName={packageName} />  </> 
             })}  
           </div>
-          <div className="bg-[#1B1125] border-[#602F92] border rounded-3xl py-6 md:py-0  h-full">
+          <div className="bg-[#1B1125] border-[#602F92]  border rounded-3xl py-6 md:py-0  h-full">
             {packageOffersData2.map(({ packageName, packageFeatures }, index) => {
               return <>  
-              {index === 0 && <div className="hidden md:sticky md:block    rounded-3xl bg-[#1B1125]   md:top-0 md:pt-6">
+              {index === 0 && <div className="hidden md:sticky md:block  md:top-[10.8rem]  rounded-3xl bg-[#1B1125]    md:pt-6 md:z-20 ">
                 <h6 className="text-[24px] ">Playroom</h6>
                 </div> }
            
@@ -43,7 +45,7 @@ const Pricing = () => {
           <div>           
           {packageOffersData3.map(({ packageName, packageFeatures }, index) => {
              return <>
-                 {index === 0 && <div className="hidden md:sticky md:block bg-black md:top-0 md:pt-8 border-b-[1.5px] pb-6 border-[#8f8d8d73]">
+                 {index === 0 && <div className="hidden md:sticky md:block  bg-black md:top-[10.3rem] md:pt-8 border-b-[1.5px] pb-6 border-[#8f8d8d73]">
                 <h6 className="text-center  text-[13px]  text-[#D2D2D2]">Others</h6>
                 </div> }
                <PackageList  className=" text-white"  key={index} packageOffres={packageFeatures} packageName={packageName} />
